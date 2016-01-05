e6da5753168724fe1c5222c832a3b94608fd6a45
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2016-01-05 20:19:32 +0000

    no message

--- src/Console/TestCommand.php
+++ src/Console/TestCommand.php
@@ -5,33 +5,16 @@ use Illuminate\Console\Command;
 
 class TestCommand extends Command
 {
-    protected $name = 'Ipboard:test';
+    protected $name = 'ipboard:test';
     protected $description = 'Make a test call using the given API settings.';
-    protected $ipboard;
 
-    public function __construct(Ipboard $ipboard)
+    public function __construct()
     {
         parent::__construct();
-
-        $this->ipboard = $ipboard;
     }
 
     public function fire()
     {
-        $members = $this->ipboard->getMembersByPage();
-
-        exit();
-        $result = $this->ipboard->hello();
-
-        if(array_get($result, "communityName", null)){
-            $this->info("Your community is online:");
-            $this->info("URL: ".$result["communityUrl"]);
-            $this->info("Name: ".$result["communityName"]);
-            $this->info("Version: ".$result["ipsVersion"]);
-            return;
-        }
-
-        $this->error("It doesn't look like your community API can be reached.  Check your config file (config/ipboard.php)");
-        $this->error("If you cannot find your config file, ensure you have published all vendor files.");
+        IPBoardAPI::basic();
     }
 }
--- src/Ipboard.php
+++ src/Ipboard.php
@@ -1,69 +1,29 @@
 <?php namespace Alawrence\Ipboard;
 
 use GuzzleHttp\Client as HttpClient;
-use GuzzleHttp\Exception\ClientException;
 
 class Ipboard
 {
     protected $url;
     protected $key;
     protected $reference;
-    protected $httpRequest;
-    private $error_exceptions = [
-        520 => \Exception::class,
-        401 => Exceptions\IpboardUnauthorized::class,
-        429 => Exceptions\IpBoardThrottled::class,
-    ];
 
     public function __construct()
     {
         $this->url = config("ipboard.api_url");
         $this->key = config("ipboard.api_key");
         $this->reference = config("ipboard.api_reference_name");
-
-        $this->httpRequest = new HttpClient([
-            "base_url" => $this->url,
-            "timeout"  => 2.0,
-            "defaults" => [
-                "auth" => [$this->key, ""],
-            ]
-        ]);
     }
 
-    public function hello()
-    {
-        return $this->getRequest("core/hello")->json();
-    }
-
-    public function getMembersByPage($sortBy="ID", $sortDir="asc", $page=1){
-        return $this->getRequest("core/members", ["query" => ["sortBy" => $sortBy, "sortDir" => $sortDir, "page" => $page]])->json();
-    }
-
-    public function getMembersAll($sortBy="ID", $sortDir="asc"){
+    public function make(){
 
     }
 
-    private function request($method, $function)
-    {
-        $response = null;
-        try {
-            $response = $this->httpRequest->{$method}($function);
-            return $response;
-        } catch(ClientException $e){
-            $this->handleError($e->getCode());
-        }
-    }
+    public function basic(){
+        $apiRequest = new HttpClient();
+        $result = $apiRequest->request("GET", $this->url."core/hello", ["auth" => [$this->key, ""]]);
 
-    private function getRequest($function){
-        return $this->request("get", $function);
-    }
-
-    private function handleError($code){
-        try {
-            throw new $this->error_exceptions[$code];
-        } catch(Exception $e){
-            throw new \Exception("There was a malformed response from IPBoard.");
-        }
+        dd($result);
     }
 
 }
