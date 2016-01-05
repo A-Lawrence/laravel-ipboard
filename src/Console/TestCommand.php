bc762e4ba61e84d298a22f700a0007357215b270
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2016-01-05 20:27:38 +0000

    no message

--- src/Console/TestCommand.php
+++ src/Console/TestCommand.php
@@ -7,14 +7,31 @@ class TestCommand extends Command
 {
     protected $name = 'ipboard:test';
     protected $description = 'Make a test call using the given API settings.';
+    protected $ipboard;
 
-    public function __construct()
+    public function __construct(Ipboard $ipboard)
     {
         parent::__construct();
+
+        $this->ipboard = $ipboard;
     }
 
     public function fire()
     {
-        IPBoardAPI::basic();
+        $members = $this->ipboard->getMembersByPage();
+
+        exit();
+        $result = $this->ipboard->hello();
+
+        if(array_get($result, "communityName", null)){
+            $this->info("Your community is online:");
+            $this->info("URL: ".$result["communityUrl"]);
+            $this->info("Name: ".$result["communityName"]);
+            $this->info("Version: ".$result["ipsVersion"]);
+            return;
+        }
+
+        $this->error("It doesn't look like your community API can be reached.  Check your config file (config/ipboard.php)");
+        $this->error("If you cannot find your config file, ensure you have published all vendor files.");
     }
 }
